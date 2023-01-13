8d75229981f847d570b25475c7ec6e9da3816894
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-13 12:20:40 +0200

    Task 3

--- src/Homework.java
+++ src/Homework.java
@@ -1,13 +1,14 @@
 public class Homework {
     public static void main(String[] args) {
 
-        float a = 27.12F;
-        long b = 987678965549L;
-        float c = 2.786F;
-        short d = 569;
-        short e = -159;
-        short f = 27897;
-        byte g = 67;
+        byte ludmilaPavlova = 23;
+        byte annaSergeevna = 27;
+        byte ekaterinaAndreevna = 30;
+
+        short totalStudent = (short) (ludmilaPavlova + annaSergeevna + ekaterinaAndreevna);
+        short totalList = 480;
+
+        System.out.println("На каждого ученика рассчитано " + totalList / totalStudent + " листов бумаги");
 
     }
 
