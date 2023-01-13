e882b2a55e1a0d113b1ad781727c4c4d717d119b
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-13 17:22:37 +0200

    Task 5

--- src/Homework.java
+++ src/Homework.java
@@ -1,17 +1,12 @@
 public class Homework {
     public static void main(String[] args) {
 
-        byte numberBottles = 16;
-        byte amountTime = 2;
-        byte twentyMinutes = 20;
-        short oneDay = 24 * 60;
-        short threeDays = 3 * 24 * 60;
-        long oneMouth = 30 * 24 * 60;
+        byte numberCans = 120;
+        byte whiteCan = 2;
+        byte brownCan = 4;
+        byte numberClasses = (byte) (numberCans / (whiteCan + brownCan));
 
-        System.out.println("За 20 минут машина произвела " + ((twentyMinutes / amountTime) * numberBottles) + " штук бутылок");
-        System.out.println("За сутки машина произвела " + ((oneDay / amountTime) * numberBottles) + " штук бутылок");
-        System.out.println("За 3 дня машина произвела " + ((threeDays / amountTime) * numberBottles) + " штук бутылок");
-        System.out.println("За 1 месяц минут машина произвела " + ((oneMouth / amountTime) * numberBottles) + " штук бутылок");
+        System.out.println("В школе, где " + (numberClasses) + " классов, нужно " + numberClasses * whiteCan + " банок белой краски и " + numberClasses * brownCan + " банок коричневой краски");
 
     }
 
