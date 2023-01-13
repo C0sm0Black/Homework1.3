8268dd44d6b8ea1c8ea0ae080a78603381fc20a9
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-13 12:12:48 +0200

    Task 2

--- src/Homework.java
+++ src/Homework.java
@@ -1,19 +1,13 @@
 public class Homework {
     public static void main(String[] args) {
 
-        int a = 123456789;
-        byte b = -128;
-        short c = 9876;
-        long d = 98765432123456789L;
-        float e = 123.12F;
-        double f = 321.455678909F;
-
-        System.out.println("Значение переменной a с типом int равно " + a);
-        System.out.println("Значение переменной b с типом byte равно " + b);
-        System.out.println("Значение переменной c с типом short равно " + c);
-        System.out.println("Значение переменной d с типом long равно " + d);
-        System.out.println("Значение переменной e с типом float равно " + e);
-        System.out.println("Значение переменной f с типом double равно " + f);
+        float a = 27.12F;
+        long b = 987678965549L;
+        float c = 2.786F;
+        short d = 569;
+        short e = -159;
+        short f = 27897;
+        byte g = 67;
 
     }
 
